63f42b555a1ee5d7d91652b624a3966a5ba41bfd
Alex <alex@Alexs-MacBook-Air.local>
2024-04-20 21:54:31 -0400

    updated TopRow

--- final-prototype-frontend/src/components/workout-builder/TopRow.tsx
+++ final-prototype-frontend/src/components/workout-builder/TopRow.tsx
@@ -1,5 +1,13 @@
 const TopRow = () => {
-  return <div>Set,Weight,Reps,Target,RPE</div>;
+  return (
+    <div className="w-full flex flex-row">
+      <span className="p-2 flex" style={{ flex: '1 1 10%' }}>Set</span>
+      <span className="p-2 flex" style={{ flex: '1 1 22.5%' }}>Weight</span>
+      <span className="p-2 flex" style={{ flex: '1 1 22.5%' }}>Reps</span>
+      <span className="p-2 flex" style={{ flex: '1 1 22.5%' }}>Target</span>
+      <span className="p-2 flex" style={{ flex: '1 1 22.5%' }}>RPE</span>
+    </div>
+  );
 };
 
 export default TopRow;
