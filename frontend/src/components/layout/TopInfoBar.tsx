df2c935340b2148f16b403b69f460353f7583e5a
Alex Williamson <arthurwilliamson96@gmail.com>
2024-04-23 00:47:33 -0400

    updated spacing of weight training at top to be centered

--- frontend/src/components/layout/TopInfoBar.tsx
+++ frontend/src/components/layout/TopInfoBar.tsx
@@ -3,15 +3,19 @@ import { Button } from '@mui/material';
 
 const TopInfoBar = () => {
   return (
-    <div className='flex justify-between items-center text-2xl'>
-      <Button variant='text'>Close</Button>
-      <span className='pb-1.5 pt-1.5 '>Weight Training</span>
-      <span className='pb-1.5 pt-1.5 pl-2 pr-2'>
+    <div className='flex justify-between items-center w-full px-2'>
+      <div className='flex-1 flex justify-start'>
+        <Button variant='text'>Close</Button>
+      </div>
+      <div className='text-center text-xl'>
+        <span>Weight Training</span>
+      </div>
+      <div className='flex-1 flex justify-end'>
         <SettingsOutlinedIcon
           className='text-orange-500'
           fontSize='medium'
         />
-      </span>
+      </div>
     </div>
   );
 };
